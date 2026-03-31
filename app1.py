4e1c5ff5b78feda56372c23d58873a1b35f26c47
V0ranc <voranc.rekar@sts.sckr.si>
2026-03-31 08:34:35 +0000

    2

--- app1.py
+++ app1.py
@@ -1,1 +1,25 @@
-from flask import Flask, render_temeplate, request, session, redirect, jsonify
+from flask import Flask, render_template, request, session, redirect, jsonify
+import sqlite3
+
+app = Flask(__name__, template_folder="templates1", ststic_folder="static1")
+app.secret_key = "skrivnost123"
+
+DB_pot="db1.sqlite3"
+#--- Baza ---
+def baza():
+    conn = sqlite3.connect('db1.sqlite3')
+    c = conn.cursor()
+    c.execute('create table if not exists user (id intiger primary key autoincrement, username text, password text)')
+    c.execute('create table if not exists notes (id intiger primary key autoincrement, context text, user_id integer)')
+    c.commit()
+    c.close()
+
+    init_db()
+
+
+#--- registracija ---
+@app.route("/reg", methods = ["GET", "POST"])
+def reg():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
