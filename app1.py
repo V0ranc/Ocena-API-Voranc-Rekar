2c3a49fb801899c02348f0e0f8b5bd3feea14e0d
V0ranc <voranc.rekar@sts.sckr.si>
2026-04-07 08:38:55 +0000

    6

--- app1.py
+++ app1.py
@@ -3,18 +3,20 @@ import sqlite3
 import bcrypt
 
 app = Flask(__name__, template_folder="templates1", static_folder="static1")
-
+app.secret_key = "6549321251"
 DB_pot="db1.sqlite3"
+
 #--- Baza ---
 def baza():
-    conn = sqlite3.connect('db1.sqlite3')
+    conn = sqlite3.connect(DB_pot)
     c = conn.cursor()
     c.execute('create table if not exists user (id integer primary key autoincrement, username text, password text)')
-    c.execute('create table if not exists notes (id integer primary key autoincrement, context text, user_id integer)')
+    c.execute('create table if not exists notes (id integer primary key autoincrement, naslov text, context text, user_id integer)')
     conn.commit()
     conn.close()
+baza()
 
-# homepage
+# --- homepage ---
 @app.route("/")
 def home():
     if "user" in session:
@@ -27,17 +29,34 @@ def reg():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"].encode('utf-8')
-        haspass = bcrypt.hashpw(password, bcrypt.gensalt()).decode('utf-8')
 
+        conn = sqlite3.connect(DB_pot)
+        c = conn.cursor()
+        c.execute('select * from user where usernamw=?', (username))
+        if c.fetchone():
+            conn.close()
+            return "Uporabnik že obstaja"
+        
+        haspass = bcrypt.hashpw(password, bcrypt.gensalt()).decode('utf-8')
+        
+        c.execute('insert into user(username, password) VALUES (?, ?)', (username, haspass))
+        conn.commit()
+        conn.close()
+
+        return redirect("/loggin")
+
+    return render_template("reg.html")
+
+#--- login ---
 @app.route("/loggin", methods = ["GET", "POST"])
 def loggin():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"].encode('utf-8')
 
-        conn = sqlite3.connect('db1.sqlite3')
+        conn = sqlite3.connect(DB_pot)
         c = conn.cursor()
-        c.execute("SELECT * FROM user where usename=?", (username,))
+        c.execute("SELECT * FROM user where usename=?", (username))
         user = c.fetchone()
         conn.close()
 
@@ -46,6 +65,32 @@ def loggin():
              return redirect("/main")
         return "Napačen login"
     return render_template("loggin.html")
+
+# --- MAIN PAGE ---
+@app.route("/main")
+def main():
+    if "user" not in session:
+        return redirect("/loggin")
+    
+    filter = request.args.get("filter")
+
+    conn = sqlite3.connect(DB_pot)
+    c = conn.cursor()
+
+    if filter:
+        c.execute('select * from notes where naslov=?', (f"%{filter}%"))
+    
+    else:
+        c.execute("select * from notes")
+
+    notes = c.fetchall()
+    conn.close()
+    return render_template("main.html", user=session["user"])
+
+
+
+
+
 
 app.run(debug=True)
        
